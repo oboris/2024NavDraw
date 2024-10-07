4377e61b38934f756a4ed772ff3178b378676f91
Boris Onyshchenko <oboris@ukr.net>
2024-10-07 15:25:55 +0300

    Add: entity annotation to Author data class

--- app/src/main/java/info/goodlift/a2024navdraw/model/Author.kt
+++ app/src/main/java/info/goodlift/a2024navdraw/model/Author.kt
@@ -1,10 +1,17 @@
 package info.goodlift.a2024navdraw.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "authors")
 data class Author(
-    val firstName : String,
-    val lastName : String,
-    val birthDate : String
+    @ColumnInfo(name = "first_name") val firstName : String,
+    @ColumnInfo(name = "last_name") val lastName : String,
+    @ColumnInfo(name = "birth_date") val birthDate : String?
 ) : ItemTypeInterface {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
     override fun getType(): Int {
         return ItemTypeInterface.AUTHOR_TYPE
     }
